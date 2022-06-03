852ada649289e2f4902b264b1dc659e2fe64b02e
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2022-06-03 10:22:56 +0000

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  define: {
+    global: {},
+  },
+  plugins: [reactRefresh()],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      port: 443,
+    }
+  }
+})
